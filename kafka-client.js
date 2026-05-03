b91bc2709f8c1bd0fd9dd620549eca107c862e61
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2026-05-04 02:17:58 +0530

    Update kafka-client.js

--- kafka-client.js
+++ kafka-client.js
@@ -1,6 +1,12 @@
 import { Kafka } from 'kafkajs';
 
 export const kafkaClient = new Kafka({
-  clientId: 'himanshu',
-  brokers: ['0.0.0.0:9092'],
+  clientId: 'location-tracker',
+  brokers:[process.env.KAFKA_BOOTSTRAP_SERVER], // E.g. 'xyz.gcp.com:9092'
+  ssl: true,
+  sasl: {
+    mechanism: 'plain', 
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD,
+  },
 });
